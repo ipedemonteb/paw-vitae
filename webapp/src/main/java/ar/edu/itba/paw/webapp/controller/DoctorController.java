f3979bee63007e3630228149c9bc46e3764f2b2e
aggalan <aggalan@itba.edu.ar>
2025-04-01 21:16:47 -0300

    doctorcontroller

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/DoctorController.java
@@ -0,0 +1,29 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.interfaceServices.DoctorService;
+import ar.edu.itba.paw.webapp.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DoctorController {
+
+    private final DoctorService ds;
+
+    @Autowired
+    public DoctorController(DoctorService ds) {
+        this.ds = ds;
+    }
+
+    @RequestMapping(value = "/{id:\\d+}", method = {RequestMethod.GET})
+    public ModelAndView getUser(@PathVariable("id") long id) {
+        final ModelAndView mav = new ModelAndView("doctor/doctor");
+        mav.addObject("doctor", ds.getById(id).orElseThrow(UserNotFoundException::new));
+        return mav;
+    }
+}
