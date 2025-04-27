777e5a746c812ac84f635098b3c1d6e3720179f1
nbellavitis <nbellavitisalzate@itba.edu.ar>
2025-04-27 14:13:10 -0300

    server side validation for appointment date

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validation/AppointmentValidDateValidator.java
@@ -0,0 +1,48 @@
+package ar.edu.itba.paw.webapp.validation;
+
+import ar.edu.itba.paw.webapp.validation.AppointmentValidDate;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class AppointmentValidDateValidator implements ConstraintValidator<AppointmentValidDate, Object> {
+
+    private String dateFieldName;
+    private String timeFieldName;
+
+    @Override
+    public void initialize(AppointmentValidDate constraintAnnotation) {
+        this.dateFieldName = constraintAnnotation.date();
+        this.timeFieldName = constraintAnnotation.startTime();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        try {
+            Field dateField = value.getClass().getDeclaredField(dateFieldName);
+            Field timeField = value.getClass().getDeclaredField(timeFieldName);
+
+            dateField.setAccessible(true);
+            timeField.setAccessible(true);
+
+            LocalDate date = (LocalDate) dateField.get(value);
+            Integer hour = (Integer) timeField.get(value);
+
+            if (date == null || hour == null) {
+                return true;
+            }
+
+            LocalDateTime appointmentDateTime = LocalDateTime.of(date, LocalTime.of(hour, 0));
+            LocalDateTime now = LocalDateTime.now();
+
+            return appointmentDateTime.isAfter(now);
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
