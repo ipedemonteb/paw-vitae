0fc519961c74e47a48664c0c1bf50250485cd1ab
Ignacio Pedemonte Berthoud <nachopedemonte@Ignacios-MacBook-Air.local>
2025-05-06 21:31:07 -0300

    added service layer testing

---
+++ services/src/test/java/ar/edu/itba/paw/services/RatingServiceImplTest.java
@@ -0,0 +1,37 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.interfacePersistence.RatingDao;
+import ar.edu.itba.paw.interfaceServices.DoctorService;
+import ar.edu.itba.paw.interfaceServices.PatientService;
+import ar.edu.itba.paw.models.Rating;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RatingServiceImplTest {
+
+    @Mock
+    private RatingDao ratingDao;
+
+    @Mock
+    private DoctorService doctorService;
+
+    @Mock
+    private PatientService patientService;
+
+    @InjectMocks
+    private RatingServiceImpl ratingService;
+
+    @Test
+    public void testCreateRating() {
+        //Preconditions
+
+        //COMO CHEQUEO EL RATING ACTUALIZADO?
+    }
+}
