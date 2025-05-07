98efb77323c3aa4e0ac78b9374844b10eefc98f1
smaffeo33 <smaffeo@itba.edu.ar>
2025-05-07 19:08:01 -0300

    validation for same pasword

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/ChangePasswordForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/ChangePasswordForm.java
@@ -1,8 +1,11 @@
 package ar.edu.itba.paw.webapp.form;
 
+import ar.edu.itba.paw.webapp.validation.RepeatPasswordMatch;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@RepeatPasswordMatch(password = "password", repeatPassword = "repeatPassword", message = "{register.passwordsDoNotMatch}")
 public class ChangePasswordForm {
 
     @NotEmpty
