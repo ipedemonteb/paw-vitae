b6ec3abaf05e25203fcdcef099028cb9ccfea87b
smaffeo33 <smaffeo@itba.edu.ar>
2025-05-08 13:52:31 -0300

    email notification when doctor rated

--- interfaceServices/src/main/java/ar/edu/itba/paw/interfaceServices/MailService.java
+++ interfaceServices/src/main/java/ar/edu/itba/paw/interfaceServices/MailService.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.interfaceServices;
 
 import ar.edu.itba.paw.models.Appointment;
+import ar.edu.itba.paw.models.Doctor;
+import ar.edu.itba.paw.models.Patient;
 import ar.edu.itba.paw.models.User;
 import org.springframework.scheduling.annotation.Async;
 
@@ -13,4 +15,6 @@ public interface MailService {
     void sendRecoverPasswordEmail(User user, String token);
 
     void sendVerificationRegisterEmail(User user, String verificationLink);
+
+    void sendRatingMail(Doctor doctor, Patient patient, Appointment appointment, long rating, String ratingMessage);
 }
