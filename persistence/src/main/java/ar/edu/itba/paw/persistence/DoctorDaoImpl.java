f7dbcc591833275ac3cab95ecf00b3147a988ef0
aggalan <aggalan@itba.edu.ar>
2025-05-09 14:00:40 -0300

    validations to prevent sql injections

--- persistence/src/main/java/ar/edu/itba/paw/persistence/DoctorDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/DoctorDaoImpl.java
@@ -199,11 +199,40 @@ public class DoctorDaoImpl implements DoctorDao {
     @Override
     public List<Doctor> getWithFilters(long specialtyId, long coverageId, List<Integer> weekdays, String orderBy, String direction, int page, int pageSize) {
         StringBuilder sql = new StringBuilder(BASE_SQL);
+
+        String orderByQuery = switch (orderBy) {
+            case "name" -> "name";
+            case "last_name" -> "last_name";
+            case "rating" -> "rating";
+            case "email" -> "email";
+            default -> "doctor_id";
+        };
+
+        String directionQuery = switch (direction) {
+            case "asc" -> "ASC";
+            case "desc" -> "DESC";
+            default -> "ASC";
+        };
+
+        long specialtyIdQuery = specialtyId > 0 ? specialtyId : 0;
+
+        long coverageIdQuery = coverageId > 0 ? coverageId : 0;
+
+        List<Integer> weekdaysQuery = new ArrayList<>();
+
+        for (Integer weekday : weekdays) {
+            if (weekday >= 0 && weekday < 7) {
+                weekdaysQuery.add(weekday);
+            }
+        }
+
+        long pageQuery = page > 0 ? page : 1;
+
         sql.append("WHERE u.id IN (SELECT doctor_id FROM users u JOIN doctors d ON u.id = d.doctor_id WHERE u.is_verified = true ");
-        List<Object> params = getObjects(specialtyId, coverageId, weekdays, sql);
-        sql.append("ORDER BY ").append(orderBy).append(" ").append(direction).append(" LIMIT ? OFFSET ?) ORDER BY ").append(orderBy).append(" ").append(direction);
+        List<Object> params = getObjects(specialtyIdQuery, coverageIdQuery, weekdaysQuery, sql);
+        sql.append("ORDER BY ").append(orderByQuery).append(" ").append(directionQuery).append(" LIMIT ? OFFSET ?) ORDER BY ").append(orderByQuery).append(" ").append(directionQuery);
         params.add(pageSize);
-        params.add((page - 1) * pageSize);
+        params.add((pageQuery - 1) * pageSize);
         return jdbcTemplate.query(sql.toString(), new DoctorExtractor(), params.toArray());
     }
 
