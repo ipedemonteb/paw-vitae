58add63b920cb0cd15b69a43f5a628ad99aa269d
aggalan <aggalan@itba.edu.ar>
2025-05-18 01:23:12 -0300

    Appointment Dao Hibernate Migration

--- persistence/src/main/java/ar/edu/itba/paw/persistence/hibernate/AppointmentDaoHibeImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/hibernate/AppointmentDaoHibeImpl.java
@@ -6,62 +6,174 @@ import ar.edu.itba.paw.models.Doctor;
 import ar.edu.itba.paw.models.Patient;
 import ar.edu.itba.paw.models.Specialty;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public class AppointmentDaoHibeImpl implements AppointmentDao {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public Appointment create(LocalDateTime date, String status, String reason, Specialty specialty, Doctor doctor, Patient patient, String report) {
-        return null;
+        Appointment appointment = new Appointment(date, status, reason, specialty, doctor, patient, report);
+        em.persist(appointment);
+        return appointment;
     }
 
+
+    //TODO: Check if necessary
     @Override
     public void cancelAppointment(long appointmentId) {
-
+//        Appointment appointment = em.find(Appointment.class, appointmentId);
+//        if (appointment != null) {
+//            appointment.setStatus("CANCELLED");
+//        }
     }
 
+    //TODO: Check if can be done from service layer
     @Override
     public void completeAppointments() {
+//        em.createQuery("UPDATE Appointment a SET a.status = 'COMPLETO' WHERE a.status = 'CONFIRMADO' AND a.date < CURRENT_TIMESTAMP").executeUpdate();
+    }
 
+    public List<Appointment> getPastConfirmedAppointments() {
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("America/Argentina/Buenos_Aires")).withMinute(5).withSecond(0).withNano(0);
+        LocalDateTime fiveDaysAgo = now.minusDays(5);
+        TypedQuery<Appointment> query = em.createQuery("FROM Appointment a WHERE a.status = 'CONFIRMADO' AND a.date BETWEEN :start AND :end", Appointment.class);
+        query.setParameter("start", fiveDaysAgo);
+        query.setParameter("end", now);
+        return query.getResultList();
     }
 
     @Override
     public Optional<Appointment> getById(long appointmentId) {
-        return Optional.empty();
+        return Optional.ofNullable(em.find(Appointment.class, appointmentId));
     }
 
     @Override
     public List<Appointment> getAppointments(long userId, boolean isFuture, int page, int size, String filter) {
-        return List.of();
+        Query nativeQuery = getNativeQuery(userId, isFuture, filter, false);
+        nativeQuery.setFirstResult((page-1) * size);
+        nativeQuery.setMaxResults(size);
+
+        List<?> appointmentIdsRaw = nativeQuery.getResultList();
+        List<Long> appointmentIds = appointmentIdsRaw.stream().map(id -> ((Number) id).longValue()).collect(Collectors.toList());
+        TypedQuery<Appointment> query = em.createQuery("FROM Appointment a WHERE a.id IN (:appointmentIds)", Appointment.class);
+        query.setParameter("appointmentIds", appointmentIds);
+
+        return query.getResultList();
     }
+
+
 
     @Override
     public List<Appointment> getAppointmentsByUserAndDate(long userId, LocalDate date, Integer time) {
-        return List.of();
+        String queryStr = "SELECT a FROM Appointment a WHERE (a.doctor.id = :userId OR a.patient.id = :userId) AND DATE(a.date) = :date";
+        TypedQuery<Appointment> query = em.createQuery(queryStr, Appointment.class)
+                .setParameter("userId", userId)
+                .setParameter("date", date);
+        return query.getResultList();
     }
 
     @Override
     public List<Appointment> getAppointmentsByDate(LocalDate today) {
-        return List.of();
+        TypedQuery<Appointment> query = em.createQuery("SELECT a FROM Appointment a WHERE DATE(a.date) = :today", Appointment.class);
+        query.setParameter("today", today);
+        return query.getResultList();
     }
 
     @Override
     public List<Appointment> getFutureAppointmentsByUser(long userId) {
-        return List.of();
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("America/Argentina/Buenos_Aires")).withMinute(5).withSecond(0).withNano(0);
+        TypedQuery<Appointment> query = em.createQuery("SELECT a FROM Appointment a WHERE (a.doctor.id = :userId OR a.patient.id = :userId) AND a.date > :now", Appointment.class);
+        query.setParameter("userId", userId);
+        query.setParameter("now", now);
+        return query.getResultList();
     }
 
     @Override
     public int countAppointments(long userId, boolean isFuture, String filter) {
-        return 0;
+        Query nativeQuery = getNativeQuery(userId, isFuture, filter, true);
+        return ((Number) nativeQuery.getSingleResult()).intValue();
     }
 
+
+    //TODO: Check if necessary
     @Override
     public void updateReport(long appointmentId, String report) {
-
+//        Appointment appointment = em.find(Appointment.class, appointmentId);
+//        if (appointment != null) {
+//            appointment.setReport(report);
+//        }
     }
+
+    //TODO: CHECK
+    private static StringBuilder getSql(boolean isFuture, String filter, boolean isCount) {
+        StringBuilder sql = new StringBuilder();
+        if (isCount) {
+            sql.append("SELECT COUNT(DISTINCT a.id) ");
+        } else {
+            sql.append("SELECT a.id ");
+        }
+        sql.append("FROM Appointments a WHERE (a.doctor_id = :userId OR a.patient_id = :userId) ");
+
+        if (isFuture) {
+            if (filter != null && !filter.isEmpty()) {
+                switch (filter) {
+                    case "today":
+                        sql.append("AND DATE(a.date) = CURRENT_DATE AND a.status <> 'cancelado' AND a.date > NOW() ");
+                        break;
+                    case "week":
+                        sql.append("AND a.date BETWEEN NOW() AND NOW() + INTERVAL '7 DAYS' AND a.status <> 'cancelado' ");
+                        break;
+                    case "month":
+                        sql.append("AND a.date BETWEEN NOW() AND NOW() + INTERVAL '1 MONTH' AND a.status <> 'cancelado' ");
+                        break;
+                    default:
+                        sql.append("AND a.date > NOW() AND a.status <> 'cancelado' ");
+                        break;
+                }
+            } else {
+                sql.append("AND a.date > NOW() AND a.status <> 'cancelado' ");
+            }
+        } else {
+            switch (filter) {
+                case "completed":
+                    sql.append("AND a.status = 'completo' AND a.date < NOW() ");
+                    break;
+                case "cancelled":
+                    sql.append("AND a.status = 'cancelado' ");
+                    break;
+                default:
+                    sql.append("AND (a.date < NOW() OR a.status = 'cancelado') ");
+                    break;
+            }
+        }
+
+        return sql;
+    }
+
+    private Query getNativeQuery(long userId, boolean isFuture, String filter, boolean isCount) {
+        StringBuilder sql = getSql(isFuture, filter, isCount);
+        Query nativeQuery = em.createNativeQuery(sql.toString());
+        nativeQuery.setParameter("userId", userId);
+        return nativeQuery;
+    }
+
+
 }
+
