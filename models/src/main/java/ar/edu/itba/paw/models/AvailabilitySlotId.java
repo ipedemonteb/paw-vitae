e94c56baa9a382b342c3825d7087b2a644345c21
aggalan <aggalan@itba.edu.ar>
2025-05-18 01:53:02 -0300

    small fix on AvailabilitySlotId

--- models/src/main/java/ar/edu/itba/paw/models/AvailabilitySlotId.java
+++ models/src/main/java/ar/edu/itba/paw/models/AvailabilitySlotId.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 import java.time.LocalTime;
@@ -8,8 +9,11 @@ import java.util.Objects;
 @Embeddable
 public class AvailabilitySlotId implements Serializable {
 
+    @Column(name = "doctor_id", nullable = false)
     private long doctorId;
+    @Column(name = "day_of_week", nullable = false)
     private int dayOfWeek;
+    @Column(name = "start_time", nullable = false)
     private LocalTime startTime;
 
     public AvailabilitySlotId() {}
