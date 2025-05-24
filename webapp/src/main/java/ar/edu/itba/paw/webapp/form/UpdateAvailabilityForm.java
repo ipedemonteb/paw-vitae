aa892f249351ffc062c17739dc3fa6bdcc45d1af
aggalan <aggalan@itba.edu.ar>
2025-05-23 23:53:57 -0300

    unavailability slots rework and empty bug

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/UpdateAvailabilityForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/UpdateAvailabilityForm.java
@@ -2,8 +2,11 @@ package ar.edu.itba.paw.webapp.form;
 
 import ar.edu.itba.paw.models.AvailabilitySlot;
 import ar.edu.itba.paw.models.AvailabilitySlotForm;
+import ar.edu.itba.paw.models.UnavailabilitySlotForm;
 import ar.edu.itba.paw.webapp.validation.TimeSlotIntersection;
+import ar.edu.itba.paw.webapp.validation.UnavailabilityIntersection;
 import ar.edu.itba.paw.webapp.validation.ValidTimeSlot;
+import ar.edu.itba.paw.webapp.validation.ValidUnavailability;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.validation.constraints.NotEmpty;
@@ -18,6 +21,19 @@ public class UpdateAvailabilityForm {
     @ValidTimeSlot(message = "{slots.invalid}")
     @TimeSlotIntersection(message = "{slots.overlap}")
     private List<AvailabilitySlotForm> availabilitySlots;
+
+    @ValidUnavailability(message = "{unavailable.slots.invalid}")
+    @UnavailabilityIntersection(message = "{unavailable.slots.overlap}")
+    private List<UnavailabilitySlotForm> unavailabilitySlots;
+
+    public List<UnavailabilitySlotForm> getUnavailabilitySlots() {
+        return unavailabilitySlots;
+    }
+
+    public void setUnavailabilitySlots(List<UnavailabilitySlotForm> unavailabilitySlots) {
+        this.unavailabilitySlots = unavailabilitySlots;
+    }
+
     public List<AvailabilitySlotForm> getAvailabilitySlots() {
         return availabilitySlots;
     }
