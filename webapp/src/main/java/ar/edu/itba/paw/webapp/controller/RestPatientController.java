afb49a94e455836dd364694186f26753eda61a11
Santiago Maffeo <smaffeo@itba.edu.ar>
2025-12-26 22:35:43 +0000

    coveragesDTO and /patient/id/coverage endpoint

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RestPatientController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RestPatientController.java
@@ -2,6 +2,7 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaceServices.PatientService;
 import ar.edu.itba.paw.models.Patient;
+import ar.edu.itba.paw.webapp.dto.CoverageDTO;
 import ar.edu.itba.paw.webapp.dto.PatientDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +34,18 @@ public class RestPatientController {
     public Response getById(@PathParam("id") final long id) {
         final Patient patient = this.patientService.getById(id).orElseThrow(NotFoundException::new);
         return Response.ok(new GenericEntity<>(PatientDTO.fromPatient(patient, uriInfo)) {}).build();
+    }
+
+    @GET
+    @Path("/{id}/coverages")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Response getCoveragesByPatientId(@PathParam("id") final long id) {
+        final Patient patient = this.patientService.getById(id).orElseThrow(NotFoundException::new);
+        if (patient.getCoverage() == null) {
+            return Response.noContent().build();
+        }
+        return Response.ok(new GenericEntity<>(CoverageDTO.fromCoverage(patient.getCoverage(), uriInfo)) {}).build();
+
     }
 
 
