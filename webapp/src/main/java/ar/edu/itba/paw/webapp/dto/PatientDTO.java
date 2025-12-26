3e0ce7ac50f72765ab840971a629b6b1061dd5c2
Santiago Maffeo <smaffeo@itba.edu.ar>
2025-12-26 21:58:13 +0000

    added PatientDTO and patient endpoint

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/PatientDTO.java
@@ -0,0 +1,99 @@
+package ar.edu.itba.paw.webapp.dto;
+
+import ar.edu.itba.paw.models.Patient;
+
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+
+public class PatientDTO {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private URI coverages;
+    private URI neighborhood;
+    private URI appointments;
+    private URI self;
+
+
+    public static PatientDTO fromPatient(Patient patient, UriInfo uriInfo) {
+        PatientDTO res = new PatientDTO();
+        res.name = patient.getName();
+        res.lastName = patient.getLastName();
+        res.email = patient.getEmail();
+        res.phone = patient.getPhone();
+
+        String patientId = String.valueOf(patient.getId());
+
+        res.coverages = uriInfo.getBaseUriBuilder().path("patients").path(patientId).path("coverages").build();
+        res.neighborhood = uriInfo.getBaseUriBuilder().path("neighborhoods").path(String.valueOf(patient.getNeighborhood().getId())).build();
+        res.appointments = uriInfo.getBaseUriBuilder().path("appointments").queryParam("patientId", patient.getId()).build();
+        res.self = uriInfo.getBaseUriBuilder().path("patients").path(patientId).build();
+        return res;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public URI getCoverages() {
+        return coverages;
+    }
+
+    public URI getNeighborhood() {
+        return neighborhood;
+    }
+
+    public URI getAppointments() {
+        return appointments;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCoverages(URI coverages) {
+        this.coverages = coverages;
+    }
+
+    public void setNeighborhood(URI neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public void setAppointments(URI appointments) {
+        this.appointments = appointments;
+    }
+
+    public URI getSelf() {
+        return self;
+    }
+
+    public void setSelf(URI self) {
+        this.self = self;
+    }
+}
