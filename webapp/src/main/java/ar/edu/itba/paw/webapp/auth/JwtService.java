21fa59ffc565beaf6bdaa18ca73f62df574a267f
Santiago Maffeo <smaffeo@itba.edu.ar>
2025-12-29 21:28:12 +0000

    Jwt auth and basic auth work with API

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtService.java
@@ -0,0 +1,126 @@
+package ar.edu.itba.paw.webapp.auth;
+
+import ar.edu.itba.paw.models.Doctor;
+import ar.edu.itba.paw.models.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+
+public class JwtService {
+
+    private final Key key;
+
+    public JwtService(Resource jwtKeyRes) throws IOException {
+        key = Keys.hmacShaKeyFor(FileCopyUtils.copyToString(new InputStreamReader(jwtKeyRes.getInputStream())).getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtService.class);
+    String ROLE_CLAIM = "role";
+    String NAME_CLAIM = "name";
+    String SELF_URL_CLAIM = "selfUrl";
+
+
+
+
+    private static final long REFRESH_TOKEN_VALIDITY = 1000L * 60 * 60 * 24 * 30;
+    private static final long ACCESS_TOKEN_VALIDITY = 1000 * 60 * 10;
+//    private static final long ACCESS_TOKEN_VALIDITY = 15000; FOR TESTS ONLY
+    private final String TOKEN_TYPE_CLAIM = "tokenType";
+
+    public String generateToken(ServletUriComponentsBuilder uriBuilder, User user, JwtTokenType type, long duration) {
+
+        return Jwts.builder()
+                .setClaims(buildClaims(uriBuilder, user, type))
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + duration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String generateAccessToken(ServletUriComponentsBuilder uriBuilder, User user) {
+        return generateToken(uriBuilder, user, JwtTokenType.ACCESS_TOKEN, ACCESS_TOKEN_VALIDITY);
+    }
+
+    public String generateRefreshToken(ServletUriComponentsBuilder uriBuilder, User user) {
+        return generateToken(uriBuilder, user, JwtTokenType.REFRESH_TOKEN, REFRESH_TOKEN_VALIDITY );
+    }
+
+    private Claims buildClaims(ServletUriComponentsBuilder uriBuilder, User user, JwtTokenType type) {
+        final Claims claims = Jwts.claims();
+        claims.put(TOKEN_TYPE_CLAIM, type.getType());
+
+        if (!type.isRefreshToken()) {
+            claims.put(NAME_CLAIM, user.getName());
+
+            //TODO: REVISAR
+
+            boolean isDoctor = user instanceof Doctor;
+            String selfPath = isDoctor ? "doctors" : "patients";
+
+
+            if (isDoctor) {
+                claims.put(ROLE_CLAIM, "ROLE_DOCTOR");
+            } else {
+                claims.put(ROLE_CLAIM, "ROLE_PATIENT");
+            }
+
+            final String selfUrl = uriBuilder
+                    .path(selfPath)
+                    .path(String.valueOf(user.getId()))
+                    .build().toString();
+
+            claims.put(SELF_URL_CLAIM, selfUrl);
+        }
+        return claims;
+    }
+
+    public JwtDetails validate(String token) {
+        try {
+            final Jws<Claims> parsed = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            final Claims claims = parsed.getBody();
+
+            if (claims.getExpiration().before(new Date(System.currentTimeMillis()))) {
+                throw new ExpiredJwtException(parsed.getHeader(), claims, "JWT token expired");
+            }
+
+            return new JwtDetails.Builder()
+                    .setId(claims.getId())
+                    .setToken(token)
+                    .setEmail(claims.getSubject())
+                    .setIssuedDate(claims.getIssuedAt())
+                    .setExpirationDate(claims.getExpiration())
+                    .setTokenType(JwtTokenType.fromCode((claims.get(TOKEN_TYPE_CLAIM).toString())))
+                    .build();
+
+        } catch (SignatureException ex) {
+            LOGGER.warn("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            LOGGER.warn("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            LOGGER.warn("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            LOGGER.warn("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            LOGGER.warn("JWT claims string is empty - {}", ex.getMessage());
+        } catch (Exception ex) {
+            LOGGER.warn("JWT claims {}", ex.getMessage());
+        }
+        return null;
+    }
+
+
+}
--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -1,13 +1,18 @@
 package ar.edu.itba.paw.webapp.config;
 
 import ar.edu.itba.paw.webapp.auth.AuthUserDetailsService;
+import ar.edu.itba.paw.webapp.auth.BasicFilter;
+import ar.edu.itba.paw.webapp.auth.JwtService;
+import ar.edu.itba.paw.webapp.auth.JwtTokenFilter;
 import ar.edu.itba.paw.webapp.handler.AuthEntryPointHandler;
 import ar.edu.itba.paw.webapp.handler.CustomAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,10 +20,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
+
+import static ar.edu.itba.paw.webapp.auth.AuthUtils.HEADER_ACCESS_TOKEN;
+import static ar.edu.itba.paw.webapp.auth.AuthUtils.HEADER_REFRESH_TOKEN;
 
 @EnableWebSecurity
 @ComponentScan("ar.edu.itba.paw.webapp.auth")
@@ -28,6 +45,12 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
     private final AuthUserDetailsService authUserDetailsService;
     private final String rememberMeSecretKey;
+
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+    @Autowired
+    private BasicFilter basicFilter;
 
     @Autowired
     public WebAuthConfig(@Lazy AuthUserDetailsService authUserDetailsService, String rememberMeSecretKey) {
@@ -55,62 +78,85 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+//    @Override
+//    protected void configure(final HttpSecurity http) throws Exception {
+//        http.sessionManagement()
+//                .invalidSessionUrl("/")
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/", "/verify", "/verify-confirmation", "/verify-result", "/email-sent", "/image/*", "/recover-password", "/change-password", "/change-password-result", "/about-us").permitAll()
+//                .antMatchers("/search", "/search/doctors/*").access("isAnonymous() or hasRole('PATIENT')")
+//                .antMatchers("/search/{id}").access("isAnonymous() or hasRole('PATIENT') or (hasRole('DOCTOR') and @accessHandler.canAccessDoctorProfile(authentication, #id))")
+//                .antMatchers("/search/").access("isAnonymous() or hasRole('PATIENT') or !hasRole('DOCTOR')")
+//                .antMatchers("/login", "/register-patient", "/register", "/email-sent").anonymous()
+//                .antMatchers("/doctor/appointments/{id}/history").access("hasRole('DOCTOR') and @accessHandler.canSeeMedicalHistory(authentication,#id)")
+//                .antMatchers("/doctor/dashboard/appointment-details/{id}").access("@accessHandler.canHandleAppointment(authentication, #id) and hasRole('DOCTOR')")
+//                .antMatchers( "/appointment/{id}/file/").access("@accessHandler.canHandleAppointment(authentication, #id)")
+//                .antMatchers("/patient/dashboard/appointment-details/{id}").access("@accessHandler.canHandleAppointment(authentication, #id) and hasRole('PATIENT')")
+//                .antMatchers("doctor/dashboard/appointment/cancel").access("@accessHandler.canHandleAppointment(authentication, request.getParameter('appointmentId')) and hasRole('DOCTOR')")
+//                .antMatchers( "patient/dashboard/appointment/cancel").access("@accessHandler.canHandleAppointment(authentication, request.getParameter('appointmentId')) and hasRole('PATIENT')")
+//                .antMatchers("/appointment/confirmation/{id}").access("hasRole('PATIENT') and @accessHandler.canHandleAppointment(authentication, #id)")
+//                .antMatchers("/appointment").access("hasRole('PATIENT') ")
+//                .antMatchers("/appointment/{appointmentId}/file-view/{fileId}", "/appointment/{appointmentId}/file/{fileId}").access("@accessHandler.hasEnabledFullMedicalHistory(authentication, #appointmentId)")
+//                .antMatchers("/doctor/dashboard/unavailability/**").access("hasRole('DOCTOR') and @accessHandler.canHandleUnavailability(request)")
+//                .antMatchers("appointment/doctor/*/availability").access("hasRole('PATIENT')")
+//                .antMatchers("/doctor/**").hasRole("DOCTOR")
+//                .antMatchers("/patient/**").hasRole("PATIENT")
+//                .antMatchers("/appointment/*/file/*").authenticated()
+//                .antMatchers("/doctors/**").anonymous()
+//                .antMatchers("/patients/**").anonymous()
+//                .antMatchers("/images/**").anonymous()
+//                .antMatchers("/appointments/**").anonymous()
+//                .antMatchers("/neighborhoods/**").anonymous()
+//                .antMatchers("/coverages/**").anonymous()
+//                .antMatchers("/specialties/**").anonymous()
+//                .antMatchers("/ratings/**").anonymous()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .usernameParameter("j_email")
+//                .passwordParameter("j_password")
+//                .loginPage("/login")
+//                .successHandler(customAuthenticationSuccessHandler())
+//                .and()
+//                .rememberMe()
+//                .rememberMeParameter("j_rememberme")
+//                .userDetailsService(authUserDetailsService)
+//                .key(rememberMeSecretKey)
+//                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login")
+//                .and()
+//                .exceptionHandling()
+//                    .authenticationEntryPoint(authEntryPointHandler())
+//                .and()
+//                .csrf()
+//                .disable();
+//    }
+
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http.sessionManagement()
-                .invalidSessionUrl("/")
-                .and()
-                .authorizeRequests()
-                .antMatchers("/", "/verify", "/verify-confirmation", "/verify-result", "/email-sent", "/image/*", "/recover-password", "/change-password", "/change-password-result", "/about-us").permitAll()
-                .antMatchers("/search", "/search/doctors/*").access("isAnonymous() or hasRole('PATIENT')")
-                .antMatchers("/search/{id}").access("isAnonymous() or hasRole('PATIENT') or (hasRole('DOCTOR') and @accessHandler.canAccessDoctorProfile(authentication, #id))")
-                .antMatchers("/search/").access("isAnonymous() or hasRole('PATIENT') or !hasRole('DOCTOR')")
-                .antMatchers("/login", "/register-patient", "/register", "/email-sent").anonymous()
-                .antMatchers("/doctor/appointments/{id}/history").access("hasRole('DOCTOR') and @accessHandler.canSeeMedicalHistory(authentication,#id)")
-                .antMatchers("/doctor/dashboard/appointment-details/{id}").access("@accessHandler.canHandleAppointment(authentication, #id) and hasRole('DOCTOR')")
-                .antMatchers( "/appointment/{id}/file/").access("@accessHandler.canHandleAppointment(authentication, #id)")
-                .antMatchers("/patient/dashboard/appointment-details/{id}").access("@accessHandler.canHandleAppointment(authentication, #id) and hasRole('PATIENT')")
-                .antMatchers("doctor/dashboard/appointment/cancel").access("@accessHandler.canHandleAppointment(authentication, request.getParameter('appointmentId')) and hasRole('DOCTOR')")
-                .antMatchers( "patient/dashboard/appointment/cancel").access("@accessHandler.canHandleAppointment(authentication, request.getParameter('appointmentId')) and hasRole('PATIENT')")
-                .antMatchers("/appointment/confirmation/{id}").access("hasRole('PATIENT') and @accessHandler.canHandleAppointment(authentication, #id)")
-                .antMatchers("/appointment").access("hasRole('PATIENT') ")
-                .antMatchers("/appointment/{appointmentId}/file-view/{fileId}", "/appointment/{appointmentId}/file/{fileId}").access("@accessHandler.hasEnabledFullMedicalHistory(authentication, #appointmentId)")
-                .antMatchers("/doctor/dashboard/unavailability/**").access("hasRole('DOCTOR') and @accessHandler.canHandleUnavailability(request)")
-                .antMatchers("appointment/doctor/*/availability").access("hasRole('PATIENT')")
-                .antMatchers("/doctor/**").hasRole("DOCTOR")
-                .antMatchers("/patient/**").hasRole("PATIENT")
-                .antMatchers("/appointment/*/file/*").authenticated()
-                .antMatchers("/doctors/**").anonymous()
-                .antMatchers("/patients/**").anonymous()
-                .antMatchers("/images/**").anonymous()
-                .antMatchers("/appointments/**").anonymous()
-                .antMatchers("/neighborhoods/**").anonymous()
-                .antMatchers("/coverages/**").anonymous()
-                .antMatchers("/specialties/**").anonymous()
-                .antMatchers("/ratings/**").anonymous()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .usernameParameter("j_email")
-                .passwordParameter("j_password")
-                .loginPage("/login")
-                .successHandler(customAuthenticationSuccessHandler())
-                .and()
-                .rememberMe()
-                .rememberMeParameter("j_rememberme")
-                .userDetailsService(authUserDetailsService)
-                .key(rememberMeSecretKey)
-                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
+    public void configure(final HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests()
+                // TODO: AGREGAR LAS RUTAS
+                .antMatchers("/images/**").permitAll()
+                .antMatchers("/**").authenticated()
+
                 .and()
                 .exceptionHandling()
-                    .authenticationEntryPoint(authEntryPointHandler())
+                .authenticationEntryPoint((request, response, ex) -> {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+                })
+
+                .and().headers().cacheControl().disable()
+
                 .and()
-                .csrf()
-                .disable();
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(basicFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .cors().and().csrf().disable();
     }
 
     @Override
@@ -128,4 +174,29 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     public AuthEntryPointHandler authEntryPointHandler() {
         return new AuthEntryPointHandler();
     }
+
+    @Bean
+    public JwtService jwtTokenUtil(@Value("classpath:jwtSecret.key") Resource jwtKeyRes) throws IOException {
+        return new JwtService(jwtKeyRes);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "HEAD", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+
+        config.setExposedHeaders(Arrays.asList(
+                HEADER_ACCESS_TOKEN,
+                HEADER_REFRESH_TOKEN,
+                "Location",
+                "Link",
+                "WWW-Authenticate"
+        ));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
 }
