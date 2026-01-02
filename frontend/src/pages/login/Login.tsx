64fd9776455d1a9870ce7ee0d3baaef1d7ddcf69
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2026-01-01 21:52:10 -0300

    fixed login and register pages padding top

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -2,17 +2,18 @@ import WelcomeCard from "../../components/WelcomeCard";
 import LoginContainer from "../../components/LoginContainer.tsx";
 import LoginCard from "../../components/LoginCard.tsx";
 
-const loginPage ="flex-1 flex items-center justify-center bg-gray-100 p-4 min-h-[calc(100vh-100px)]";
+const loginPage =
+    "flex-1 pt-[130px] pb-[30px] flex items-center justify-center bg-gray-100 min-h-[calc(100vh-100px)]";
 
 function Login() {
 
     return (
-            <div className={loginPage}>
-                <LoginContainer>
-                    <WelcomeCard />
-                    <LoginCard />
-                </LoginContainer>
-            </div>
+        <div className={loginPage}>
+            <LoginContainer>
+                <WelcomeCard />
+                <LoginCard />
+            </LoginContainer>
+        </div>
     )
 }
 
