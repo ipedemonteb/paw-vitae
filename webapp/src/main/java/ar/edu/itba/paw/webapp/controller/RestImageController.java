e066391cc6f576768b0511a6d1ddfb9c6961135d
aggalan <aggalan@itba.edu.ar>
2026-01-04 18:31:53 -0300

    Added POST endpoint to /images

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RestImageController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RestImageController.java
@@ -2,24 +2,32 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaceServices.ImageService;
 import ar.edu.itba.paw.models.Images;
+import ar.edu.itba.paw.webapp.utils.FileUtils;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.Set;
 
 @Path("/images")
 @Component
 public class RestImageController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RestImageController.class);
+    private static final Set<String> ALLOWED_IMAGE_TYPES = Set.of("image/png", "image/jpeg", "image/jpg");
 
     private final ImageService imageService;
+    @Context
+    private UriInfo uriInfo;
 
 
     @Autowired
@@ -36,4 +44,11 @@ public class RestImageController {
         return Response.ok(imageData).build(); //TODO: SHOULD I CACHE?
     }
 
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadImage(final FormDataMultiPart multipart) {
+        MultipartFile file = FileUtils.requireSingleImage(multipart, "file");
+        Images image = imageService.create(file);
+        return Response.created(uriInfo.getAbsolutePathBuilder().path(String.valueOf(image.getId())).build()).build();
+    }
 }
