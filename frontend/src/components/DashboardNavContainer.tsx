362bd680f9a5ebc93bce0aad1864174ee293a4cd
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2026-01-05 21:41:45 -0300

    merged dashboard

--- frontend/src/components/DashboardNavContainer.tsx
+++ frontend/src/components/DashboardNavContainer.tsx
@@ -7,7 +7,7 @@ interface AppointmentContainerProps {
 export default function DashboardNavContainer({children}: AppointmentContainerProps) {
     return (
         <div
-            className="mt-25 w-full max-w-300 rounded-xl flex flex-col items-center justify-center p-6 gap-4 bg-white shadow h-full"
+            className=" w-full max-w-300 rounded-xl flex flex-col items-center justify-center p-6 gap-4 bg-white shadow h-full"
         >
             {children}
         </div>
