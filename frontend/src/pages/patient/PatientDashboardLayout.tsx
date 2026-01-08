be549a19f8d755c692d026b41fcf80e6bf4de1ec
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2026-01-08 00:29:36 -0300

    added first part of appointment details page

--- frontend/src/pages/patient/PatientDashboardLayout.tsx
+++ frontend/src/pages/patient/PatientDashboardLayout.tsx
@@ -1,27 +1,13 @@
 import { Card } from "@/components/ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.tsx";
 import { ButtonGroup } from "@/components/ui/button-group.tsx";
-import { Mail, Phone, Calendar, History, ClipboardPlus, User } from "lucide-react";
+import { Calendar, History, ClipboardPlus, User } from "lucide-react";
 import { Button } from "@/components/ui/button.tsx";
 import {Link, Outlet, useMatch} from "react-router-dom";
 import { cn } from "@/lib/utils";
+import PatientProfileCard from "@/components/PatientProfileCard.tsx";
 
 const dashboardCointainer =
     "flex flex-col mt-36 px-5 mx-auto max-w-6xl w-full gap-6";
-const profileCard =
-    "flex flex-col gap-0 items-center sm:flex-row";
-const avatarContainer =
-    "flex items-center w-20 h-20 mx-6 mb-2 border border-[var(--primary-light)] border-4 rounded-full sm:mb-0";
-const userDataContainer =
-    "flex flex-col items-center md:items-start";
-const userName =
-    "text-[var(--text-color)] text-xl font-[700] mb-1";
-const dataContainer =
-    "flex flex-row gap-5 text-sm text-[var(--text-light)]";
-const contactData =
-    "flex flex-row items-center gap-1";
-const contactIcon =
-    "w-4 h-4";
 const sectionCard =
     "p-0 overflow-hidden shadow-[var(--shadow-md)]";
 const tabsGroup =
@@ -65,25 +51,7 @@ function DashboardTab({ to, end, icon: Icon, children }: {
 function PatientDashboardLayout() {
     return (
         <div className={dashboardCointainer}>
-            <Card className={profileCard}>
-                <Avatar className={avatarContainer}>
-                    <AvatarImage src="https://picsum.photos/200" />
-                    <AvatarFallback>JD</AvatarFallback>
-                </Avatar>
-                <div className={userDataContainer}>
-                    <h1 className={userName}>John Doe</h1>
-                    <div className={dataContainer}>
-                        <div className={contactData}>
-                            <Mail className={contactIcon} />
-                            <p className="max-w-[150px] truncate sm:max-w-[300px] sm:truncate">johndoe@gmail.com</p>
-                        </div>
-                        <div className={contactData}>
-                            <Phone className={contactIcon} />
-                            <p>11 1234-5678</p>
-                        </div>
-                    </div>
-                </div>
-            </Card>
+            <PatientProfileCard />
             <Card className={sectionCard}>
                 <ButtonGroup orientation="horizontal" className={tabsGroup}>
                     <DashboardTab to="/patient/dashboard/upcoming" end icon={Calendar}>
