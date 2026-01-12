e452b32d8a446b1cfeee9620a7f0469f053cfd14
Santiago Maffeo <smaffeo@itba.edu.ar>
2026-01-12 18:45:46 +0000

    login migrated to tan stack query

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,8 +1,31 @@
 import {useContext} from "react";
 import {AuthContext} from "@/context/authContext.tsx";
+import {setAuth} from "@/context/auth-store.ts";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import type {AxiosError} from "axios";
+import {login} from "@/data/auth.ts";
 
 export function useAuth () {
     const context = useContext(AuthContext);
     if (!context) throw new Error("useAuth must be used within an AuthProvider");
     return context;
+}
+
+
+export function useLogin() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({ email, password }: {email: string, password: string}) =>
+            login(email, password),
+
+        onSuccess: (data) => {
+            setAuth(data.jwt, data.refresh);
+
+            queryClient.invalidateQueries({ queryKey: ["auth"] });
+        },
+        onError: (error: AxiosError) => {
+            console.error("Login falló", error);
+        }
+    });
 }
