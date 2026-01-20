d435f782db35f7e72ec337edbda598e061891dc8
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2026-01-20 19:54:32 -0300

    added internationalization to medical-history page in patient dashboard

--- frontend/src/pages/patient/PatientMedicalHistory.tsx
+++ frontend/src/pages/patient/PatientMedicalHistory.tsx
@@ -8,10 +8,10 @@ function PatientMedicalHistory() {
     const { t } = useTranslation();
     return (
         <DashboardNavContainer>
-            <DashboardNavHeader title="Medical History">
+            <DashboardNavHeader title={t("medical-history.title")}>
                 <div className="flex items-center sm:justify-center gap-2 mt-2 sm:mt-0">
                     <span className="flex font-normal text-sm items-center justify-center text-(--text-light)">
-                        Sort By:
+                        {t("medical-history.sort.title")}
                     </span>
                     <NativeSelect className="font-normal cursor-pointer">
                         <NativeSelectOption value="oldest">{t("medical-history.sort.old-first")}</NativeSelectOption>
