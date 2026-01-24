a494448dba84a6ac1d4b467adf89ed7fb1fcea95
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2026-01-24 02:36:56 -0300

    progress

--- frontend/src/components/EditOfficeDialog.tsx
+++ frontend/src/components/EditOfficeDialog.tsx
@@ -27,7 +27,6 @@ export default function EditOfficeDialog({office}: EditOfficeModalProps) {
     const {data: specialties, isLoading: isLoadingSpecialties} = useSpecialties()
     const {data: currentSpecialties, isLoading: isLoadingCurrentSpecialties} = useSpecialtiesByUrl(officeSpecialties?.map(s => s.specialty))
     const [open, setOpen] = useState(false)
-    // const [removed, setRemoved] = useState(false)
 
     const form = useForm<EditOfficeForm>({
         resolver: zodResolver(EditOfficeSchema),
@@ -132,7 +131,7 @@ export default function EditOfficeDialog({office}: EditOfficeModalProps) {
                                 {(isLoading || isLoadingCurrentSpecialties || isLoadingSpecialties) ? (
                                     <div className="w-full flex flex-wrap gap-y-1 gap-x-0.5 ">
                                         {Array.from({length: 15}).map((_, i) => (
-                                            <Skeleton key={i} className="w-26 h-7 rounded-full"/>
+                                            <Skeleton key={i} className="w-28 h-8 rounded-full"/>
                                         ))}
                                     </div>
                                 ) : (
