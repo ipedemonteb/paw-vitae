ad5830473668ff762e4211296369d8974ad43611
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2026-02-02 00:18:08 -0300

    fix

--- frontend/src/lib/office-schema.ts
+++ frontend/src/lib/office-schema.ts
@@ -2,18 +2,15 @@ import { z } from "zod";
 
 //TODO internacionalizar, jeje
 
-export const EditOfficeSchema = z.object({
-    name: z.string().min(1, "Office name is required"),
-    neighborhood: z.string(),
-    active: z.boolean(),
-    specialties: z.array(z.string()).min(1, "At least one specialty is required"),
-});
-
 export const CreateOfficeSchema = z.object({
     name: z.string().min(1, "Office name is required"),
     neighborhood: z.string(),
     specialties: z.array(z.string()).min(1, "At least one specialty is required"),
 });
+
+export  const EditOfficeSchema = CreateOfficeSchema.extend({
+    active: z.boolean()
+})
 
 
 export type EditOfficeForm = z.infer<typeof EditOfficeSchema>;
