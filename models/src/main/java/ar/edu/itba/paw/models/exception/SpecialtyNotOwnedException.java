e455c203579c4b06b7e8559a99a9e0e2f213089c
aggalan <aggalan@itba.edu.ar>
2026-02-03 23:59:27 -0300

    Fixed call to action emails

--- models/src/main/java/ar/edu/itba/paw/models/exception/SpecialtyNotOwnedException.java
+++ models/src/main/java/ar/edu/itba/paw/models/exception/SpecialtyNotOwnedException.java
@@ -3,5 +3,5 @@ package ar.edu.itba.paw.models.exception;
 public class SpecialtyNotOwnedException extends CustomRuntimeException {
 
     public SpecialtyNotOwnedException() {
-        super("exception.specialtyNotOwned",ExceptionUtils.FORBIDDEN_STATUS_CODE);    }
+        super("exception.specialtyNotOwned",ExceptionUtils.BAD_REQUEST_STATUS_CODE);    }
 }
