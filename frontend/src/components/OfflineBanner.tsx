5b67361e1bd1cc0bf19055fadfc7f46ff0392a08
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2026-02-05 15:00:26 -0300

    fix

--- frontend/src/components/OfflineBanner.tsx
+++ frontend/src/components/OfflineBanner.tsx
@@ -1,12 +1,17 @@
 import {useIsOfflineLike, useOnlineStatus} from "@/hooks/useOnline";
+import {useState} from "react";
+import {TriangleAlert, X} from "lucide-react";
 
 export default function OfflineBanner() {
     const online = useOnlineStatus();
     const isOfflineLike = useIsOfflineLike()
+    const [open, setOpen] = useState(true)
 
     return (
-        <div className={`w-full h-full z-0 relative  text-center text-sm py-2 transition-all bg-(--warning-light) ${(online && !isOfflineLike) ? "-translate-y-10 opacity-0" : "  translate-y-0 opacity-100 border-b border-b-(--warning-medium)"}`}>
-            {!online ? "You’re offline. Some actions may not work." : "Network unreachable"}
+        <div className={`w-full h-full gap-4 z-0 flex items-center justify-center relative duration-300 text-sm py-2 transition-all bg-(--warning-light) ${((online && !isOfflineLike) || !open) ? "-translate-y-10 opacity-0" : "  translate-y-0 opacity-100 border-b border-b-(--warning-medium)"}`}>
+            <TriangleAlert className="text-(--warning) size-5"/>
+            <p>{!online ? "You’re offline. Some actions may not work." : "Network unreachable"}</p>
+            <X className="absolute right-2 cursor-pointer size-5" onClick={() => setOpen(false)} />
         </div>
     );
 }
