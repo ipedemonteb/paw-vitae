ef0bbba818f91b5e6355c664ad071c8a841097ba
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2026-02-06 18:56:47 -0300

    handled isLoading cases in patient dashboard and fixed header requesting doctor image and getting 404

--- frontend/src/components/PatientProfileCard.tsx
+++ frontend/src/components/PatientProfileCard.tsx
@@ -1,10 +1,8 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.tsx";
 import { Mail, Phone } from "lucide-react";
 import { Card } from "@/components/ui/card.tsx";
-import {usePatientById} from "@/hooks/usePatients.ts";
-import {Skeleton} from "@/components/ui/skeleton.tsx";
-import {Spinner} from "@/components/ui/spinner.tsx";
 import {initialsFallback} from "@/utils/userUtils.ts";
+import type {PatientDTO} from "@/data/patients.ts";
 
 const profileCard =
     "flex flex-col gap-0 items-center sm:flex-row";
@@ -21,28 +19,9 @@ const contactData =
 const contactIcon =
     "w-4 h-4";
 
-function PatientProfileCard({patientId}:{patientId: string}) {
+function PatientProfileCard({patient}:{patient: PatientDTO | undefined}) {
 
-    const { data: patient, isLoading, isError } = usePatientById(patientId);
-
-    if (isLoading) {
-        return (
-            <Skeleton className={profileCard + "w-full h-32 justify-center items-center"}>
-                <Spinner className="h-8 w-8 text-(--text-light)"/>
-            </Skeleton>
-        );
-    }
-
-    if (isError || !patient) {
-        return (
-            //TODO: handle in another way
-            <Card className={profileCard}>
-                <div className="px-6 py-4 text-[var(--danger)]">No se pudo cargar el paciente.</div>
-            </Card>
-        );
-    }
-
-    const avatarFallbackText = initialsFallback(patient.name, patient.lastName);
+    const avatarFallbackText = initialsFallback(patient?.name, patient?.lastName);
 
     return (
         <Card className={profileCard}>
@@ -51,15 +30,15 @@ function PatientProfileCard({patientId}:{patientId: string}) {
                 <AvatarFallback>{avatarFallbackText}</AvatarFallback>
             </Avatar>
             <div className={userDataContainer}>
-                <h1 className={userName}>{patient.name + " " + patient.lastName}</h1>
+                <h1 className={userName}>{patient?.name + " " + patient?.lastName}</h1>
                 <div className={dataContainer}>
                     <div className={contactData}>
                         <Mail className={contactIcon} />
-                        <p className="max-w-[150px] truncate sm:max-w-[300px] sm:truncate">{patient.email}</p>
+                        <p className="max-w-[150px] truncate sm:max-w-[300px] sm:truncate">{patient?.email}</p>
                     </div>
                     <div className={contactData}>
                         <Phone className={contactIcon} />
-                        <p>{patient.phone}</p>
+                        <p>{patient?.phone}</p>
                     </div>
                 </div>
             </div>
