8f25b3afa47b8f808f0e0a84df36af016c80e1b3
Santiago Maffeo <smaffeo@itba.edu.ar>
2026-02-07 23:31:16 +0000

    useCoverages tests

--- frontend/src/__test__/setup/index.ts
+++ frontend/src/__test__/setup/index.ts
@@ -1,7 +1,9 @@
 import { appointmentHandlers } from '../mocks/AppointmentsApiMock';
 import {authHandlers} from "@/__test__/mocks/AuthApiMock.ts";
+import {coveragesHandlers} from "@/__test__/mocks/CoveragesApiMock.ts";
 
 export const handlers = [
     ...appointmentHandlers,
-    ...authHandlers
+    ...authHandlers,
+    ...coveragesHandlers
 ];
