dd1e7ea41427ca64ad8c984b4d80ebc47575e7df
Ignacio Pedemonte Berthoud <ipedemonteb@gmail.com>
2026-02-10 11:54:27 -0300

    finished fixing search page styling

--- frontend/src/components/SearchGridCard.tsx
+++ frontend/src/components/SearchGridCard.tsx
@@ -10,6 +10,9 @@ import {initialsFallback} from "@/utils/userUtils.ts";
 import {generatePath, Link} from "react-router-dom";
 import {extractIdFromUrl} from "@/lib/utils.ts";
 import {useTranslation} from "react-i18next";
+import {useDelayedBoolean} from "@/utils/queryUtils.ts";
+import {Skeleton} from "@/components/ui/skeleton.tsx";
+import {Spinner} from "@/components/ui/spinner.tsx";
 
 const cardContainer =
     "p-0 gap-0 h-full flex flex-col justify-between ";
@@ -33,26 +36,43 @@ const scheduleButton =
     "w-full bg-[var(--primary-color)] border border-[var(--primary-color)] text-white py-2 px-4 hover:bg-[var(--primary-dark)] hover:border-[var(--primary-dark)] cursor-pointer";
 const viewProfileButton =
     "w-full bg-white text-[var(--primary-color)] border border-[var(--primary-color)] py-2 px-4 hover:text-white hover:bg-[var(--primary-dark)] hover:border-[var(--primary-dark)] cursor-pointer";
+const skeletonBadge =
+    "h-4 w-20 rounded-md";
 
 function SearchGridCard({doctor}: SearchCardProps) {
     const avatarFallbackText = initialsFallback(doctor?.name, doctor?.lastName);
     const {t} = useTranslation();
-    const specialties = useDoctorSpecialties(doctor.specialties);
-    const {url:imageUrl} = useDoctorImageUrl(doctor.self.split("/").pop());
+    const { data: specialties, isLoading: loadingSpecialties } = useDoctorSpecialties(doctor.specialties);
+    const { url:imageUrl, isLoading: loadingDoctorImgUrl } = useDoctorImageUrl(doctor.self.split("/").pop());
     const doctorId = extractIdFromUrl(doctor.self)
     const profilePath = generatePath("/profile/:id", { id: String(doctorId) })
     const schedulePath = generatePath("/appointment/:id", { id: String(doctorId) })
+
     return (
         <Card className={cardContainer}>
             <div className={iconContainer}>
-                <Avatar className={icon}>
-                    <AvatarImage src={imageUrl || undefined} />
-                    <AvatarFallback>{avatarFallbackText}</AvatarFallback>
-                </Avatar>
+                {loadingDoctorImgUrl ?
+                    <Skeleton className={`${icon} flex justify-center items-center rounded-full`}>
+                        <Spinner className="h-6 w-6 text-(--gray-300)"/>
+                    </Skeleton>
+                    :
+                    <Avatar className={icon}>
+                        <AvatarImage src={imageUrl || undefined} />
+                        <AvatarFallback>{avatarFallbackText}</AvatarFallback>
+                    </Avatar>
+                }
             </div>
             <div className={dataContainer}>
                 <h3 className={dataName}>{doctor.name} {doctor.lastName}</h3>
-                <SearchSpecialtyBadgeComponent specialties={specialties.data || []} maxDisplay={2}/>
+                {useDelayedBoolean(loadingSpecialties) ?
+                    <div className="flex gap-2 my-1">
+                        <Skeleton className={skeletonBadge} />
+                        <Skeleton className={skeletonBadge} />
+                        <Skeleton className="h-4 w-8 rounded-md" />
+                    </div>
+                    :
+                    <SearchSpecialtyBadgeComponent specialties={specialties || []} maxDisplay={2}/>
+                }
                 <div className={dataContact}>
                     <Mail className={dataIcon} />
                     <p>{doctor.email}</p>
