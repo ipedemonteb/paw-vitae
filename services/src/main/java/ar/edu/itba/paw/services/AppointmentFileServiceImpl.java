c3fb32e612afa7973e523aeb68a1fd36c7b28ec1
aggalan <aggalan@itba.edu.ar>
2026-02-15 22:44:34 -0300

    Now files restar appointment last-updated

--- services/src/main/java/ar/edu/itba/paw/services/AppointmentFileServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/AppointmentFileServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -48,6 +49,7 @@ public class AppointmentFileServiceImpl implements AppointmentFileService {
                 MailDTO dto = new MailDTO(appointment);
                 mailService.sendFileUploadMail(dto, List.of(createdFile));
             }
+            appointment.setLastModified(LocalDateTime.now());
             LOGGER.info("Appointment file created successfully for appointment with id {}", appointment_id);
             return createdFile;
         } catch (IOException e) {
