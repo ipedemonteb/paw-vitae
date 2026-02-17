16df60b62b8eb70a3970ce65532e9f2d303efbeb
nbellavitis <nbellavitisalzate@itba.edu.ar>
2026-02-17 19:23:31 -0300

    fix

--- services/src/main/java/ar/edu/itba/paw/services/DoctorServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/DoctorServiceImpl.java
@@ -10,6 +10,7 @@ import ar.edu.itba.paw.models.exception.UserNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,7 +33,7 @@ public class DoctorServiceImpl implements DoctorService {
 
     @Autowired
     public DoctorServiceImpl(DoctorDao doctorDao, PasswordEncoder passwordEncoder, ImageService imageService,
-                             SpecialtyService specialtyService, CoverageService coverageService, DoctorOfficeService doctorOfficeService,UserService userService,DoctorProfileService doctorProfileService) {
+                             SpecialtyService specialtyService, CoverageService coverageService, DoctorOfficeService doctorOfficeService,UserService userService,@Lazy DoctorProfileService doctorProfileService) {
         this.doctorDao = doctorDao;
         this.passwordEncoder = passwordEncoder;
         this.imageService = imageService;
